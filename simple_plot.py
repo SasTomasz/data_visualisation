8f2ee469686980747f8482d3bac9515528a7d8d5
Tomasz Sas <sastomasz@gmail.com>
2022-04-11 16:29:53 +0200

    Simple plot polishing

--- simple_plot.py
+++ simple_plot.py
@@ -1,7 +1,7 @@
 from bokeh.io import output_file, show
 from bokeh.plotting import figure
 
-f = figure()
+f = figure(plot_width = 400, plot_height = 500, tools = 'pan, box_zoom')
 
 output_file('my_plot.html')
 
@@ -10,5 +10,15 @@ x_axis = [1, 2, 3, 4, 5]
 y_axis = [6, 7, 8, 9, 10]
 
 
-f.circle(x_axis, y_axis)
+f.line(x_axis, y_axis)
+
+# Polishing plot, simple exaples of different polishing options
+f.title.text = "My data"
+f.title.text_color = "lime"
+f.title.text_font = "times"
+f.title.text_font_style = "bold"
+
+f.xaxis.axis_label = "x_axis"
+f.yaxis.axis_label = "y_axis"
+
 show(f)
